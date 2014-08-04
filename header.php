745caa42f54399da95e6c447fe0c9c86cb89060d
NagataHiroaki <ryushion.hyou@gmail.com>
2014-08-04 15:26:57 +0900

    パーツ分け

---
+++ header.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="ja-JP">
+<head>
+	<link rel="stylesheet" href="css/common.css">
+	<meta charset="UTF-8">
+	<meta name="description" content="">
+	<title>test</title>
+</head>
+<body class="home">
+	<header id="common-header" class="cf">
+		<div class="inner">
+			<div id="logo">
+				<a href="">タイトル</a>
+			</div>
+			<div id="nav">
+				<div id="gnav">
+					<ul>
+						<li><a href="">カテゴリ</a></li>
+						<li><a href="">検索</a></li>
+						<li><a href="">サインアップ</a></li>
+						<li><a href="">ログイン</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="mainVisual">
+			<div class="inner">
+				<div class="fl">
+					<h2>キャッチコピーエリア</h2>
+					<p>○○○は旅行に興味を持っているユーザが、旅行をするまでの手助けをするサービスです。○○○は旅行に興味を持っているユーザが、旅行をするまでの手助けをするサービスです。○○○は旅行に興味を持っているユーザが、旅行をするまでの手助けをするサービスです。</p>
+				</div>
+				<div class="fr">
+					<h2>新規ユーザ登録</h2>
+					<a href="">メールで登録</a>
+					<a href="">Instagramで登録</a>
+				</div>
+			</div>
+		</div>
+		<!-- #mainVisual -->
+	</header>
--- index.php
+++ index.php
@@ -1,43 +1,4 @@
-<!doctype html>
-<html lang="ja-JP">
-<head>
-	<link rel="stylesheet" href="css/common.css">
-	<meta charset="UTF-8">
-	<meta name="description" content="">
-	<title>test</title>
-</head>
-<body class="home">
-	<header id="common-header" class="cf">
-		<div class="inner">
-			<div id="logo">
-				<a href="">タイトル</a>
-			</div>
-			<div id="nav">
-				<div id="gnav">
-					<ul>
-						<li><a href="">カテゴリ</a></li>
-						<li><a href="">検索</a></li>
-						<li><a href="">サインアップ</a></li>
-						<li><a href="">ログイン</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<div id="mainVisual">
-			<div class="inner">
-				<div class="fl">
-					<h2>キャッチコピーエリア</h2>
-					<p>○○○は旅行に興味を持っているユーザが、旅行をするまでの手助けをするサービスです。○○○は旅行に興味を持っているユーザが、旅行をするまでの手助けをするサービスです。○○○は旅行に興味を持っているユーザが、旅行をするまでの手助けをするサービスです。</p>
-				</div>
-				<div class="fr">
-					<h2>新規ユーザ登録</h2>
-					<a href="">メールで登録</a>
-					<a href="">Instagramで登録</a>
-				</div>
-			</div>
-		</div>
-		<!-- #mainVisual -->
-	</header>
+<?php include 'header.php'; ?>
 	<div id="contents">
 		<section class="search">
 			<div class="inner">
@@ -234,26 +195,4 @@
 		</section>
 	</div>
 	<!-- /#contents -->
-	<footer id="common-footer">
-		<div class="inner">
-			<div id="sitemap">
-				<ul>
-					<li><a href="">トップ</a></li>
-					<li><a href="">カテゴリ</a></li>
-					<li><a href="">検索</a></li>
-					<li><a href="">プライバシーポリシー</a></li>
-					<li><a href="">ヘルプ</a></li>
-					<li><a href="">サインアップ</a></li>
-					<li><a href="">ログイン</a></li>
-					<li><a href="">Twitter</a></li>
-					<li><a href="">Facebook</a></li>
-				</ul>
-			</div>
-			<p id="copy">
-				<small>&copy;Service inc.</small>
-			</p>
-		</div>
-	</footer>
-	<!-- /#common-footer -->
-</body>
-</html>
+<?php include 'footer.php'; ?>

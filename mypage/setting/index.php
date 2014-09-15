5f9ad25d0821dc46b9bdb0c5fe3d1a995be7662b
NagataHiroaki <ryushion.hyou@gmail.com>
2014-09-15 17:41:25 +0900

    メインコンテンツエリア以外適用

--- mypage/setting/index.php
+++ mypage/setting/index.php
@@ -1,3 +1,73 @@
-<?php
-
-?>
+<?php include '../../header.php'; ?>
+	<div id="contents">
+			<div class="inner">
+				<div id="main">
+				</div>
+				<div id="sub">
+					<div class="user-area">
+						<div class="user-head">
+							<div class="txt">プロフィール</div>
+							<div class="user-edit"><a href="/mypage/setting/index.php">編集</a></div>
+						</div>
+						<div class="user-info">
+							<div class="user-img"><img src="/images/user-icon-default.png" width="60" height="" alt=""></div>
+							<div class="user-name">Nagata Hiroaki</div>
+							<div class="location">京都</div>
+						</div>
+						<div class="user-meta">
+							<ul>
+								<li>
+									<a href="/mypage/follow/index.php">
+										<p class="txt">フォロー</p>
+										<p class="num">100</p>
+									</a>
+								</li>
+								<li>
+									<a href="/mypage/follow/index.php">
+										<p class="txt">フォロワー</p>
+										<p class="num">80</p>
+									</a>
+								</li>
+								<li>
+									<a href="/mypage/favorite/index.php">
+										<p class="txt">お気に入り</p>
+										<p class="num">6</p>
+									</a>
+								</li>
+								<li>
+									<a href="/mypage/album/index.php">
+										<p class="txt">アルバム</p>
+										<p class="num">8</p>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="sub-button">
+						<a href="">新規投稿</a>
+					</div>
+					<div class="sub-button">
+						<a href="">フレンドを探す</a>
+					</div>
+					<div class="comment-history">
+						<div class="ttl">あなたのコメント履歴</div>
+						<ul>
+							<li>
+								<div class="txt"><a href="">私もそこに行ってみたいです。</a><span class="time">（9月14日）</span></div>
+							</li>
+							<li>
+								<div class="txt"><a href="">そのお店は定休日はございますか。</a><span class="time">（9月14日）</span></div>
+							</li>
+							<li>
+								<div class="txt"><a href="">近辺に住んでいる私の友人に聞いたところ、観光客は少なく穴場スポットであるようです。</a><span class="time">（9月14日）</span></div>
+							</li>
+						</ul>
+						<div class="more">
+							<a href="">もっと見る</a>
+						</div>
+					</div>
+				</div>
+			</div>
+	</div>
+	<!-- /#contents -->
+<?php include '../../footer.php'; ?>

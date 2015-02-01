5c59fa1aa58b9d1d72512e10e92b02592a927ba6
NagataHiroaki <ryushion.hyou@gmail.com>
2015-02-01 19:01:46 +0900

    /posts/view.php　削除 /posts/index.php　追加 /header.php　修正

--- header.php
+++ header.php
@@ -9,23 +9,9 @@
   </nav>
   <nav class="gnav type-off">
     <ul>
-      <li class="menu-search"><a href="/search/">コンテンツを探す</a></li><li class="menu-posts"><a href="/posts/add.php">コンテンツを投稿</a></li><li class="menu-my"><a href="/mypage/">マイページ</a></li><li class="menu-in"><a href="#">ログイン</a></li>
+      <!-- ここから修正箇所 1日 19:00 -->
+      <li class="menu-search"><a href="/search/">コンテンツを探す</a></li><li class="menu-in"><a href="#">ログイン</a></li>
+      <!-- ここまで修正箇所 1日 19:00 -->
     </ul>
   </nav>
-  <?php if($body_class=='home' ): ?>
-  <div id="mainVisual">
-    <div class="inner">
-      <div class="fl">
-        <h2>キャッチコピーエリア</h2>
-        <p>○○○は旅行に興味を持っているユーザが、旅行をするまでの手助けをするサービスです。○○○は旅行に興味を持っているユーザが、旅行をするまでの手助けをするサービスです。○○○は旅行に興味を持っているユーザが、旅行をするまでの手助けをするサービスです。</p>
-      </div>
-      <div class="fr">
-        <h2>新規ユーザ登録</h2>
-        <a href="/signup/index.php">メールで登録</a>
-        <a href="/signup/index.php">Instagramで登録</a>
-      </div>
-    </div>
-  </div>
-  <!-- #mainVisual -->
-  <?php endif; ?>
 </header>
